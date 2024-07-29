d2f0530d5a9ec3f33d3412bd0840456992f00eb9
cheesestraws <rob@midworld.co.uk>
2024-07-29 22:12:37 +0100

    usage message

--- cmd/scb/main.go
+++ cmd/scb/main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var args []string
 
 func usage() {
-	fmt.Printf("(help message goes here)\n")
+	fmt.Printf("%s fetch <kind> <device> <user> <pass> - print configuration to stdout\n", os.Args[0])
+	fmt.Printf("%s [-into <dir>] download <kind> <device> <user> <pass> - download a single configuration\n", os.Args[0])
+	fmt.Printf("%s [-into <dir>] download-all <tomlfile> - download configs for all devices in tomlfile\n", os.Args[0])
+	fmt.Printf("%s toml-check <tomlfile> - check approximate validity of toml file\n", os.Args[0])
+	fmt.Printf("\n")
+	flag.PrintDefaults()
 }
 
 var into *string
 
 func main() {
+	flag.Usage = usage
 	into = flag.String("into", ".", "put files in this dir")
 	flag.Parse()
 
